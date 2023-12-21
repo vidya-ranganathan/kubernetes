98e4a6a8ac56cccb7386ee59b5e1941ed69dd3e6
vidya-ranganathan <win4vidya@gmail.com>
2023-12-21 21:05:54 +0530

    Create main.go

---
+++ operators/kluster/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"kclusters/pkg/apis/cumulonimbus.ai/v1alpha1"
+)
+
+func main() {
+	kObj := v1alpha1.Kluster{}
+	fmt.Println("invoking kluster operator ... ", kObj)
+
+}
